702a557bcc694f8a0e7560a99e51d7810d475183
ArifJan <51852933+ArifJanMC@users.noreply.github.com>
2023-05-25 01:46:26 +1000

    Update keepup.py
    Refactoring the ping function: 
    This function performs a GET request to the server and logs the status. It also catches two exceptions but it repeats the logging and return statement for both of them. This can be improved by catching both exceptions at once.
    
    Refactoring the reboot function: 
    This function is quite large and does many things. It can be broken down into smaller functions to improve readability and maintainability.
    
    Reusing HTTP sessions: 
    The script creates a new HTTP session for each request. This is not necessary and can be improved by reusing the session.
    
    Configuration variables: 
    The script uses global variables for configuration. It would be better to encapsulate them into a configuration class or a configuration file.
    
    Error handling: 
    The script only logs the status of the server and whether the reboot was successful or not. It would be good to add more error handling, for example, if the server does not return a JSON response.
    
    Internationalization: 
    The script uses both English and Russian in the logs. It would be better to stick to one language for consistency.

--- keepup.py
+++ keepup.py
@@ -1,79 +1,92 @@
 import asyncio
 import aiohttp
+from aiohttp.client_exceptions import ServerDisconnectedError
+from asyncio.exceptions import TimeoutError
 
-minutes = 1  # delay between checks
-ip = '1.1.1.1'  # server ip
+class Config:
+    def __init__(self):
+        self.minutes = 1  # delay between checks
+        self.ip = '1.1.1.1'  # server ip
+        self.vmmanager_id = ''  # server id from vm.aeza.net
+        self.aeza_id = ''  # server id from my.aeza.net
+        self.login = ''  # login/email from my.aeza.net
+        self.password = ''  # password from my.aeza.net
+        self.session = aiohttp.ClientSession()
 
-vmmanager_id = ''  # server id from vm.aeza.net
-aeza_id = ''  # server id from my.aeza.net
+    async def close(self):
+        await self.session.close()
 
-login = ''  # login/email from my.aeza.net
-password = ''  # password from my.aeza.net
+config = Config()
 
-
-async def ping(ip):
-  async with aiohttp.ClientSession() as session:
+async def ping():
     try:
-      async with session.get('http://' + ip, timeout=1) as resp:
-        print(resp.status)
-        return True
-    except aiohttp.client_exceptions.ServerDisconnectedError as e:
-      print(repr(e))
-      print('Server offline')
-      return False
-    except asyncio.exceptions.TimeoutError as e:
-      print(repr(e))
-      print('Server offline')
-      return False
+        async with config.session.get(f'http://{config.ip}', timeout=1) as resp:
+            print(resp.status)
+            return True
+    except (ServerDisconnectedError, TimeoutError) as e:
+        print(repr(e))
+        print('Server offline')
+        return False
 
-
-async def reboot():
-  async with aiohttp.ClientSession() as session:
+async def get_auth_key():
     headers = {'authorization': 'Bearer undefined'}
-    json = {'method': 'credentials', 'email': login, 'password': password}
-    async with session.post('https://core.aeza.net/api/auth?',
-                            headers=headers,
-                            json=json) as resp:
-      json_ = await resp.json()
-    api_key = json_['data']['session']
-    headers = {'authorization': f'Bearer {api_key}'}
-    async with session.get(
-        f'https://core.aeza.net/api/services/{aeza_id}/goto?',
-        headers=headers) as resp:
-      json_ = await resp.json()
-    keyvm = json_['data'].split('key/')[1]
-    json = {'key': keyvm}
-    async with session.post('https://vm.aeza.net/auth/v3/auth_by_key',
-                            json=json) as resp:
-      json_ = await resp.json()
+    json = {'method': 'credentials', 'email': config.login, 'password': config.password}
+    async with config.session.post('https://core.aeza.net/api/auth?',
+                                    headers=headers,
+                                    json=json) as resp:
+        json_ = await resp.json()
+    return json_['data']['session']
+
+async def get_vm_key(auth_key):
+    headers = {'authorization': f'Bearer {auth_key}'}
+    async with config.session.get(
+            f'https://core.aeza.net/api/services/{config.aeza_id}/goto?',
+            headers=headers) as resp:
+        json_ = await resp.json()
+    return json_['data'].split('key/')[1]
+
+async def start_vm(vm_key, auth_key):
+    json = {'key': vm_key}
+    async with config.session.post('https://vm.aeza.net/auth/v3/auth_by_key',
+                                    json=json) as resp:
+        json_ = await resp.json()
     sesskey = json_['session']
     real_keyvm = json_['token']
     cookies = {'token': real_keyvm, 'ses6': sesskey}
     headers = {'x-xsrf-token': real_keyvm}
-    async with session.post(
-        f'https://vm.aeza.net/vm/v3/host/{vmmanager_id}/start',
-        cookies=cookies,
-        headers=headers) as resp:
-      respjson = await resp.json()
-  if 'id' in respjson:
-    print('УСПЕХ! Машинка автоматически запущена!')
-  else:
-    print(f'Ошибка. Не удалось запустить виртуальную машину. JSON-ответ: {respjson}')
+    async with config.session.post(
+            f'https://vm.aeza.net/vm/v3/host/{config.vmmanager_id}/start',
+            cookies=cookies,
+            headers=headers) as resp:
+        respjson = await resp.json()
+    if 'id' in respjson:
+        print('Success! The virtual machine has been automatically started.')
+    else:
+        print(f'Error. Failed to start the virtual machine. JSON response: {respjson}')
 
+async def reboot():
+    auth_key = await get_auth_key()
+    vm_key = await get_vm_key(auth_key)
+    await start_vm(vm_key, auth_key)
 
 async def main():  #reboot server if ping error 5 times in a row
-  error_count = 0
+    error_count = 0
 
-  while True:
-    for i in range(5):
-      if await ping(ip):
-        error_count = 0
-      else:
-        error_count += 1
-        if error_count == 5:
-          await reboot()
-          break
-    await asyncio.sleep(60 * minutes)
+    while True:
+        for i in range(5):
+            if await ping():
+                error_count = 0
+            else:
+                error_count += 1
+                if error_count == 5:
+                    await reboot()
+                    break
+        await asyncio.sleep(60 * config.minutes)
 
+async def run():
+    try:
+        await main()
+    finally:
+        await config.close()
 
-asyncio.run(main())
+asyncio.run(run())
