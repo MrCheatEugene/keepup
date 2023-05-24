a7c4fbdeaa59baa54e8fef11d67abba06b0781b8
ArifJan <51852933+ArifJanMC@users.noreply.github.com>
2023-05-24 23:49:01 +1000

    Update keepup.py

--- keepup.py
+++ keepup.py
@@ -1,34 +1,58 @@
 import requests
-# НАСТРОЙКИ
 
-host_id="000000" # Номер машины в VMManager (указан в списке машин, писать без #)
-aeza_id="000000" # Номер машины в панели Aeza (указан в URL, https://my.aeza.net/services/000000)
+# Constants
+BASE_URL = "https://core.aeza.net"
+VM_URL = "https://vm.aeza.net"
+AUTHORIZATION = "Bearer undefined"
 
-type_auth="login" # Тип входа (login - по логину и паролю, direct - API ключ напрямую)
+# Settings
+host_id = "000000"  # VMManager Machine ID
+aeza_id = "000000"  # Machine ID in Aeza panel
+auth_type = "login"  # Type of login: 'login' (by username and password) or 'direct' (API key directly)
 
-# login
-login="admin@example.org" # e-mail 
-password="12345678" # пароль
+# Login credentials
+email = "admin@example.org"
+password = "12345678"
 
-#direct
-key="AAAAAAAAAAAAAAAAAAAAA"
+# Direct API key
+api_key = "AAAAAAAAAAAAAAAAAAAAA"
+
+def get_auth_key():
+    response = requests.post(f"{BASE_URL}/api/auth?",
+                             json={"method": "credentials", "email": email, "password": password},
+                             headers={"authorization": AUTHORIZATION})
+    return response.json()['data']['session']
+
+def get_vm_key(auth_key):
+    response = requests.get(f"{BASE_URL}/api/services/346290/goto?",
+                            headers={"authorization": f"Bearer {auth_key}"})
+    return response.json()['data'].replace(f"{VM_URL}/auth/key/", "")
+
+def authenticate_with_vm_key(vm_key):
+    response = requests.post(f"{VM_URL}/auth/v3/auth_by_key",
+                             headers={"cookie": f"_ym_d=1669657718; _ym_uid=1669657718468356443; ref=344585; _ym_isad=1; _ym_visorc=w; token={api_key}; ses6={vm_key}",
+                             "json": {"key": vm_key}})
+    return response.json()
+
+def start_vm(real_key_vm, session_key):
+    response = requests.post(f"{VM_URL}/vm/v3/host/{host_id}/start",
+                             headers={"cookie": f"_ym_d=1669657718; _ym_uid=1669657718468356443; ref=344585; _ym_isad=1; _ym_visorc=w; token={real_key_vm}; ses6={session_key}",
+                             "x-xsrf-token": real_key_vm})
+    return response.json()
 
 try:
-	r = requests.get("http://1.1.1.1") # Тут любой удобный метод, на проверку машины. Суть в том, чтобы в случае неудачи оно выплюнуло Exception :) 
-	# Лично я тут указываю IP адрес сервера, и накатываю на сервер NGINX. Самый простой метод, как по мне
+    requests.get("http://1.1.1.1")  # Check the machine status
 except:
-	if type_auth=="login":
-		key = requests.post("https://core.aeza.net/api/auth?",json={"method": "credentials", "email": login, "password": password},headers={"authorization": "Bearer undefined"}).json()['data']['session']
-	r=requests.get("https://core.aeza.net/api/services/346290/goto?",headers={"authorization": "Bearer "+key})
-	keyvm = r.json()['data'].replace("https://vm.aeza.net/auth/key/","")
-	r = requests.post("https://vm.aeza.net/auth/v3/auth_by_key",headers={"cookie":"_ym_d=1669657718; _ym_uid=1669657718468356443; ref=344585; _ym_isad=1; _ym_visorc=w; token="+key+"; ses6="+keyvm},json={"key":keyvm})
-	sess = r.json()
-	sesskey = sess['session']
-	real_keyvm = sess['token']
-	resp = requests.post("https://vm.aeza.net/vm/v3/host/"+host_id+"/start",headers={"cookie":"_ym_d=1669657718; _ym_uid=1669657718468356443; ref=344585; _ym_isad=1; _ym_visorc=w; token="+real_keyvm+"; ses6="+sesskey,"x-xsrf-token":real_keyvm})
-	respjson = resp.json()
-	if('id' in respjson):
-		print("УСПЕХ! Машинка автоматически запущена!")
-	else:
-		print("Ошибка. Не удалось запустить виртуальную машину. JSON-ответ:")
-		print(respjson)
+    if auth_type == "login":
+        auth_key = get_auth_key()
+    vm_key = get_vm_key(auth_key)
+    session = authenticate_with_vm_key(vm_key)
+    session_key = session['session']
+    real_key_vm = session['token']
+    response_json = start_vm(real_key_vm, session_key)
+
+    if 'id' in response_json:
+        print("SUCCESS! The machine has been automatically started!")
+    else:
+        print("Error. Failed to start the virtual machine. JSON response:")
+        print(response_json)
